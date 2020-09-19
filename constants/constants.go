94cf8157fd6443772c1a64a6be3076033ca6fa88
Sneha <satputesneha@outlook.com>
2020-09-19 16:22:43 +0800

    numeric constants

--- constants/constants.go
+++ constants/constants.go
@@ -2,8 +2,22 @@ package main
 
 import "fmt"
 
-const a = 3
+const (
+	a     = 3
+	big   = 1 << 60
+	small = big >> 59
+)
+
+func needInt(x int) int {
+	return x*10 + 1
+
+}
+func needFloat(y float64) float64 {
+	return y*0.1 + 1
+}
 
 func main() {
-	fmt.Println(a)
+
+	fmt.Println(a, needInt(small), needFloat(small))
+	fmt.Println(a, needInt(big), needFloat(big))
 }
