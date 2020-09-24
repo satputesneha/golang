9cedc90056349066c4e2f9fe421bbfcc942fdc1e
Sneha <satputesneha@outlook.com>
2020-09-24 23:55:26 +0800

    slice

---
+++ slice/slice.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func sliceInfo(slice []int) {
+	fmt.Printf("len=%v,cap=%v,val=%v\n ", len(slice), cap(slice), slice)
+
+}
+func main() {
+	s := []int{0, 1, 2, 3, 4, 5, 6, 7}
+	sliceInfo(s)
+	s = s[:0]
+	sliceInfo(s)
+	s = s[:4]
+	sliceInfo(s)
+	s = s[2:]
+	sliceInfo(s)
+}
