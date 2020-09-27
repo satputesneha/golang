396b4a7957e2ef5578b98ea50993946b1a777e91
Sneha <satputesneha@outlook.com>
2020-09-28 00:35:02 +0800

    fibonacci

--- febonacci/febonacci.go
+++ febonacci/febonacci.go
@@ -3,23 +3,26 @@ package main
 import "fmt"
 
 func createFeb() func() int {
-	isFistTime := false
-	isSecondTime := false
 	first := 1
+	isFirstTime := false
 	second := 1
+	isSecondTime := false
+
 	return func() int {
-		if !isFistTime {
-			isFistTime = true
+		if !isFirstTime {
+			isFirstTime = true
 			return 1
 		} else if !isSecondTime {
 			isSecondTime = true
 			return 1
+
 		} else {
 			result := first + second
 			first = second
 			second = result
 			return result
 		}
+
 	}
 }
 
