b195f4c9a32f048e1574eddf32309f360b6bc836
Sneha <satputesneha@outlook.com>
2020-09-30 23:47:59 +0800

    exercise-stringer

---
+++ exercise-stringer/exercise-stringer.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type IPAddr [4]byte
+
+func (i IPAddr) String() string {
+	fmt.Println("string called")
+
+	return fmt.Sprintf("result: %v.%v.%v.%v", i[0], i[1], i[2], i[3])
+
+}
+
+// TODO: Add a "String() string" method to IPAddr.
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+		fmt.Println(ip)
+
+	}
+}
