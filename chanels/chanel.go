d92b45486fcc7a203869008c775685f5acdc2fd3
Sneha <satputesneha@outlook.com>
2020-10-01 23:58:54 +0800

    Go routines

--- chanels/chanel.go
+++ chanels/chanel.go
@@ -9,7 +9,22 @@ func main() {
 	fmt.Println(<-c)
 	fmt.Println(<-c)
 
+	d := make(chan int, 3)
+	intchan(3, d)
+	fmt.Println("3")
+	intchan(8, d)
+	fmt.Println("8")
+	intchan(9, d)
+	fmt.Println("9")
+
+	//fmt.Println(<-d)
+	fmt.Println("Hello")
+
 }
 func add(m string, c chan string) {
 	c <- m
+}
+
+func intchan(m int, d chan int) {
+	d <- m
 }
