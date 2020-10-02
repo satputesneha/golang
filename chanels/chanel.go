32fc8c0b87bb155ad57e72ebd2b5af407f19c11c
Sneha <satputesneha@outlook.com>
2020-10-02 16:51:34 +0800

    range-fibonacci

--- chanels/chanel.go
+++ chanels/chanel.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	c := make(chan string)
@@ -10,11 +13,11 @@ func main() {
 	fmt.Println(<-c)
 
 	d := make(chan int, 3)
-	intchan(3, d)
+	go intchan(3, d)
 	fmt.Println("3")
-	intchan(8, d)
+	go intchan(8, d)
 	fmt.Println("8")
-	intchan(9, d)
+	go intchan(9, d)
 	fmt.Println("9")
 
 	fmt.Println(<-d)
@@ -22,12 +25,19 @@ func main() {
 	fmt.Println(<-d)
 
 	fmt.Println("Hello")
-
+	go delayed()
 }
 func add(m string, c chan string) {
 	c <- m
 }
 
 func intchan(m int, d chan int) {
+	fmt.Println("intchan function")
 	d <- m
+}
+
+func delayed() {
+	time.Sleep(time.Second * 2)
+	fmt.Println("delayed")
+
 }
---
+++ range-fibonacci/range-fibonacci.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	c := make(chan int)
+	go fib(5, c)
+	for i := range c {
+		fmt.Println(i)
+	}
+
+}
+func fib(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+
+		c <- x
+		x, y = y, x+y
+
+	}
+	close(c)
+}
