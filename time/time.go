1cf939a9d23057a688b12760459291a184d73027
Sneha <satputesneha@outlook.com>
2020-10-03 19:00:38 +0800

    select with time.After channel response

--- time/time.go
+++ time/time.go
@@ -6,11 +6,17 @@ import (
 )
 
 func main() {
+	i := 0
 	for {
+		fmt.Println("Forever")
+
 		select {
-		case <-time.After(2 * time.Second):
+		case <-time.After(2 * time.Microsecond):
 			fmt.Println("exiting")
 			return
+		default:
+			i++
+			fmt.Println(i)
 		}
 	}
 }
