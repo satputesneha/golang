24dff93d3d57b0fcdf7e01e4b4490134b7375741
Sneha <satputesneha@outlook.com>
2020-10-04 23:44:07 +0800

    mutex program

---
+++ muxlock/muxlock.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var m int
+var mut sync.Mutex
+
+func calc(i int) {
+	mut.Lock()
+	defer mut.Unlock()
+	fmt.Println(i)
+	m = m + i
+	fmt.Println(i)
+	m = m * i
+	fmt.Println(i)
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		go calc(i)
+	}
+	time.Sleep(3 * time.Second)
+}
