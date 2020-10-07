1a34fecb0c9fcd450e30e95ef84b7686d00cbe40
Sneha <satputesneha@outlook.com>
2020-10-07 16:49:14 +0800

    userAgent

--- rest-api/main.go
+++ rest-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -10,11 +11,18 @@ import (
 func YourHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello Rest API\n"))
 }
+func AbcHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello Rest API\n"))
+}
+func AbcdefHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("sneha %v", r.UserAgent())))
+}
 
 func main() {
 	r := mux.NewRouter()
 	// Routes consist of a path and a handler function.
 	r.HandleFunc("/", YourHandler)
+	r.HandleFunc("/Abcdef", AbcdefHandler)
 
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe(":8000", r))
