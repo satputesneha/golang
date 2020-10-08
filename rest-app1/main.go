3da90050bfda467181d095c1b24e16de75e6a228
Sneha <satputesneha@outlook.com>
2020-10-09 01:02:04 +0800

    shop-list

--- rest-app1/main.go
+++ rest-app1/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -11,6 +12,7 @@ import (
 func YourHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	s := fmt.Sprintf("<html><h1>Super Market</h1>hello Rest API %v\n</html>", vars)
+	time.Sleep(10 * time.Second)
 	w.Write([]byte(s))
 }
 
@@ -19,7 +21,7 @@ func main() {
 	// Routes consist of a path and a handler function.
 	r.HandleFunc("/products/{key1}/{key2}", YourHandler)
 	r.HandleFunc("/category/{catname1}", YourHandler)
-	//r.HandleFunc("/category/{catname1}", YourHandler)
+	r.HandleFunc("/shopping-list/{shoplistnuum:[0-9]+}", YourHandler)
 
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe(":8000", r))
