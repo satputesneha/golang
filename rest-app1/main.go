51e1292a416f717eea6ffb03e49820f1477f0c1d
Sneha <satputesneha@outlook.com>
2020-10-09 00:05:36 +0800

    category

---
+++ rest-app1/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	s := fmt.Sprintf("<html><h1>Super Market</h1>hello Rest API %v\n</html>", vars)
+	w.Write([]byte(s))
+}
+
+func main() {
+	r := mux.NewRouter()
+	// Routes consist of a path and a handler function.
+	r.HandleFunc("/products/{key1}/{key2}", YourHandler)
+	r.HandleFunc("/category/{catname1}", YourHandler)
+	//r.HandleFunc("/category/{catname1}", YourHandler)
+
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
