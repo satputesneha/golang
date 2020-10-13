adab8cdfeb29455aaabc4a4b3707657103fb79e6
Sneha <satputesneha@outlook.com>
2020-10-13 23:11:28 +0800

    spliting into files

--- events/events.go
+++ events/events.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,42 +8,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
-
-type Event struct {
-	Id    int    `json:"id"`
-	Title string `json:"title"`
-	Host  string `json:"host"`
-}
-
-type restApp struct {
-	db *sql.DB
-}
-
-func (app *restApp) eventHandler(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	rows, err := app.db.Query("select * from events where id=" + params["event_id"])
-	e := Event{}
-	if err != nil {
-		json.NewEncoder(w).Encode(&e)
-		return
-	}
-	rows.Next()
-
-	rows.Scan(&e.Id, &e.Title, &e.Host)
-	json.NewEncoder(w).Encode(&e)
-}
-
-func (app *restApp) initialise() {
-	var err error
-	app.db, err = sql.Open("mysql", "root:yourpassword@tcp(127.0.0.1:3306)/events_db")
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (app *restApp) teardown() {
-	app.db.Close()
-}
 
 func main() {
 	fmt.Println("Welcome to rest app")
