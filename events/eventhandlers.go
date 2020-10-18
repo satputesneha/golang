814cb658409bff1b8ae16046d49af902df1507fb
Sneha <satputesneha@outlook.com>
2020-10-18 15:31:45 +0800

    EventHandler

---
+++ events/eventhandlers.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Event struct {
+	Id    int    `json:"id"`
+	Title string `json:"title"`
+	Host  string `json:"host"`
+}
+
+func (app *restApp) EventHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	rows, err := app.db.Query("select * from events where id=" + params["event_id"])
+	e := Event{}
+	if err != nil {
+		json.NewEncoder(w).Encode(&e)
+		return
+	}
+	rows.Next()
+
+	rows.Scan(&e.Id, &e.Title, &e.Host)
+	json.NewEncoder(w).Encode(&e)
+}
--- events/users.go
+++ events/users.go
@@ -14,25 +14,7 @@ type Users struct {
 	Country string `json:"country"`
 	Age     int    `json:"age"`
 }
-type Event struct {
-	Id    int    `json:"id"`
-	Title string `json:"title"`
-	Host  string `json:"host"`
-}
 
-func (app *restApp) EventHandler(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	rows, err := app.db.Query("select * from events where id=" + params["event_id"])
-	e := Event{}
-	if err != nil {
-		json.NewEncoder(w).Encode(&e)
-		return
-	}
-	rows.Next()
-
-	rows.Scan(&e.Id, &e.Title, &e.Host)
-	json.NewEncoder(w).Encode(&e)
-}
 func (app *restApp) UserHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("welcome UserHandler")
 	params := mux.Vars(r)
