de6152edd9c2fee0649968406625f5670606c21f
Sneha <satputesneha@outlook.com>
2020-10-18 15:17:22 +0800

    Registrations

--- events/restapp.go
+++ events/restapp.go
@@ -60,62 +60,6 @@ func (app *restApp) UserHandler(w http.ResponseWriter, r *http.Request) {
 	rows.Scan(&u.UserID, &u.Name, &u.Country, &u.Age)
 	json.NewEncoder(w).Encode(&u)
 }
-func (app *restApp) RegHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("welcome to RegHandler")
-	params := mux.Vars(r)
-	rows, err := app.db.Query("select reg_id,event_id,user_id from registration where user_id =?", params["user_id"])
-	reg := Registration{}
-	if err != nil {
-		log.Println(err)
-		json.NewEncoder(w).Encode(&reg)
-		return
-	}
-	rows.Next()
-	rows.Scan(&reg.RegID, &reg.EventID, &reg.UserID)
-	json.NewEncoder(w).Encode(&reg)
-
-}
-func (app *restApp) AllRegByEventHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("welcome to AllRegByEventHandler")
-	params := mux.Vars(r)
-	rows, err := app.db.Query("select reg_id,event_id,user_id from registration where event_id =?", params["event_id"])
-	reg := Registration{}
-	if err != nil {
-		log.Println(err)
-		json.NewEncoder(w).Encode(&reg)
-		return
-	}
-	allregbyevent := make([]Registration, 0)
-	for rows.Next() {
-
-		rows.Scan(&reg.RegID, &reg.EventID, &reg.UserID)
-		allregbyevent = append(allregbyevent, reg)
-
-	}
-	json.NewEncoder(w).Encode(&allregbyevent)
-}
-
-func (app *restApp) AllRegHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("welcome to AllRegHandler")
-	params := mux.Vars(r)
-	rows, err := app.db.Query("select reg_id,event_id,user_id from registration where user_id =?", params["user_id"])
-	reg := Registration{}
-	if err != nil {
-		log.Println(err)
-		json.NewEncoder(w).Encode(&reg)
-		return
-	}
-	allreg := make([]Registration, 0)
-
-	for rows.Next() {
-		rows.Scan(&reg.RegID, &reg.EventID, &reg.UserID)
-		allreg = append(allreg, reg)
-
-	}
-
-	json.NewEncoder(w).Encode(&allreg)
-
-}
 
 func (app *restApp) Initialise() {
 	var err error
