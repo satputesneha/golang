4257273b37b988e6b767f31f08bd1aa0e3c58642
Sneha <satputesneha@outlook.com>
2020-10-22 22:05:51 +0800

    checkb

--- expression_match/expression_match.go
+++ expression_match/expression_match.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("***")
-	fmt.Println(checke("{[()]}"))
-	fmt.Println(checke("{[[)]}"))
+	fmt.Println(checkb("{[()]}"))
+	fmt.Println(checkb("{[[)]}"))
 
 }
 
@@ -42,6 +42,34 @@ func checke(photo string) bool {
 
 		fmt.Println(person, stack)
 
+	}
+	return true
+}
+func checkb(photo string) bool {
+	stack := make([]string, 0)
+	m := make(map[string]string)
+	m["{"] = "}"
+	m["["] = "]"
+	m["("] = ")"
+
+	for i := 0; i < len(photo); i++ {
+		person := string(photo[i])
+		_, exists := m[person]
+		if exists {
+			stack = append(stack, person)
+		} else {
+			if len(stack) == 0 {
+				return false
+			}
+			top := stack[len(stack)-1]
+			if person == m[top] {
+				stack = stack[:len(stack)-1]
+			} else {
+				return false
+			}
+
+		}
+		fmt.Println(person, stack)
 	}
 	return true
 }
