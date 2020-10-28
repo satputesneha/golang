ff0ce49e9152face70faf6847ced0ffc51c56ea6
Sneha <satputesneha@outlook.com>
2020-10-28 23:56:20 +0800

    checking one's & 0's int

--- reverse/main.go
+++ reverse/main.go
@@ -20,9 +20,25 @@ func sum(x int) int {
 	}
 	return result
 }
+func ones(x int) bool {
+
+	result := 0
+	for ; x != 0; x = x / 10 {
+		fmt.Println(x, result)
+		result = x % 10
+		if result == 0 || result == 1 {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+
+}
 
 func main() {
 
 	fmt.Println(reverse(234))
 	fmt.Println(sum(234))
+	fmt.Println(ones(1010))
 }
