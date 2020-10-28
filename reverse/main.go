b45c761c49c601a6186a48a6a08b2fcf74d4e690
Sneha <satputesneha@outlook.com>
2020-10-28 23:49:44 +0800

    sum of int

--- reverse/main.go
+++ reverse/main.go
@@ -3,13 +3,26 @@ package main
 import "fmt"
 
 func reverse(x int) int {
+
 	result := 0
 	for ; x != 0; x = x / 10 {
+		fmt.Println(x, result)
 		result = result*10 + x%10
 	}
 	return result
 }
+func sum(x int) int {
+
+	result := 0
+	for ; x != 0; x = x / 10 {
+		fmt.Println(x, result)
+		result = result + x%10
+	}
+	return result
+}
+
 func main() {
 
 	fmt.Println(reverse(234))
+	fmt.Println(sum(234))
 }
